feda9b305c68bb805c00c19b2fe25fb1b5548444
EmmeM <emmemcdonald66@gmail.com>
2026-04-25 19:04:32 -0500

    add abstract minigame classes

--- src/game/minigames/Minigame.ts
+++ src/game/minigames/Minigame.ts
@@ -1,4 +1,6 @@
 import type { GameScene } from "../GameScene";
+import type { MinigameScene } from "./MinigameScene";
+import { TugOfWarScene } from "./tugOfWar/TugOfWarScene";
 
 export type MinigameDefinition = {
 	id: string;
@@ -6,7 +8,7 @@ export type MinigameDefinition = {
 	description: string;
 };
 
-export const SLEEPOVER_MINIGAMES: MinigameDefinition[] = [
+export const MINIGAMES: MinigameDefinition[] = [
 	{
 		id: 'tow',
 		name: 'Tug of War',
@@ -14,21 +16,36 @@ export const SLEEPOVER_MINIGAMES: MinigameDefinition[] = [
 	},
 ];
 
+export const MINIGAME_SCENES: Record<string, typeof MinigameScene> = {
+  tow: TugOfWarScene
+};
+
 export function pickRandomMinigame(): MinigameDefinition {
-	const randomIndex = Math.floor(Math.random() * SLEEPOVER_MINIGAMES.length);
-	return SLEEPOVER_MINIGAMES[randomIndex];
+	const randomIndex = Math.floor(Math.random() * MINIGAMES.length);
+	return MINIGAMES[randomIndex];
 }
 
 export abstract class Minigame {
 	requestId: string;
 	gameScene: GameScene;
+	minigameScene: MinigameScene | null;
 	def: MinigameDefinition;
 
-	constructor(requestId: string, gameScene: GameScene, def: MinigameDefinition) {
+	constructor(
+		requestId: string, 
+		gameScene: GameScene, 
+		def: MinigameDefinition,
+	) {
 		this.requestId = requestId;
 		this.gameScene = gameScene;
 		this.def = def;
+		this.minigameScene = null;
+		const minigameSceneType = MINIGAME_SCENES[this.def.id];
+		if (minigameSceneType) {
+			//@ts-ignore
+			this.minigameScene = new minigameSceneType(this.def.id, this.gameScene);
+		}
 	}
 
-	abstract setupSocketListeners: () => void;
+	abstract setupSocketListeners(): void;
 }
